b6ae000d2744792c79184807b8e097a569ecf50a
Tijani <teejayadewale87@gmail.com>
2025-05-31 02:33:10 +0100

    modify home page

--- static/js/custom_typing.js
+++ static/js/custom_typing.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // The text you want to type
-    var textToType = "At Lagos Web Dev, we help Businesses grow online with expert web development and SEO services. From custom Django web applications to responsive WordPress websites and Shopify e-commerce stores, we build solutions that drive traffic, improve visibility, and generate results. Explore our portfolio to see what we’ve built.";
+    var textToType = "At Lagos Web Dev, we specialize in helping businesses grow online. As leading web developers in Lagos, we offer expert web development and SEO services designed to enhance your digital presence. From custom Django web applications to responsive WordPress websites and powerful Shopify e-commerce stores, we build solutions that not only drive traffic and improve search visibility but also generate tangible results for your business. Explore our portfolio to see the successful web development projects we've delivered for various clients across Lagos and beyond.";
 
     var options = {
         strings: [textToType],
